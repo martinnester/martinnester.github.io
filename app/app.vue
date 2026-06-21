98f263d8a70c677caa47ce09f1933558e5a612e7
Martin Nester <martinn7352@gmail.com>
2026-06-20 23:50:37 -0400

    first commit

---
+++ app/app.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <p>
+            Hello, I'm Martin Nester. a software developer from Virginia, United
+            States. I'm most familiar with web dev, but also enjoy operating
+            systems, distrubited programming, information retrieval, chasos
+            engineering, local-first, databases, functional programming,
+            language design, DX/UI/UX, game development, and more.
+        </p>
+        <p>
+            There is so much out there I want to learn about, change, contribute
+            to and improve. I hope to start sharing my progress here.
+        </p>
+        <br />
+        <p>Recently I've been most interested in:</p>
+        <ul>
+            <li><a href="https://www.ripple-ts.com/">Ripple TS</a></li>
+            <li><a href="https://www.unison-lang.org/">Unison</a></li>
+            <li><a href="https://electric.ax/sync/">Electric Sync</a></li>
+        </ul>
+    </main>
+</template>
+
+<style scoped>
+main {
+    margin: auto;
+    padding: 1em;
+    max-width: 50em;
+    font-family:
+        -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial,
+        sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+}
+</style>
